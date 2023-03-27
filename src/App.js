58952a8870ea13ee912a681c3f36437e68dd5de7
hijasabdul <hijas.abdulsalam@i-o-tech.net>
2023-03-27 10:22:21 +0400

    separate components for category list and item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './categories.styles.scss'
+import CategoryList from './components/category-list/category-list';
+import CategoryItem from './components/category/category-item';
 
 const App = () => {
 
@@ -34,19 +36,7 @@ const App = () => {
 
 
   return (
-    <div className='categories-container'>
-      {categories.map(({title,id,imageUrl}) => (
-      <div key={id} className='category-container'>
-        <div className='background-image' style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}/>
-        <div className='category-body-container'>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      ))}
-    </div>  
+    <CategoryList categories={categories}></CategoryList>
   );
 
 
