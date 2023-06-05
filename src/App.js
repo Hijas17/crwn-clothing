328996b120fb1d6ba534e17a541d5cf2e132c4df
hijasabdul <hijas.abdulsalam@i-o-tech.net>
2023-06-05 12:02:19 +0400

    all changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import Home from './routes/homepage/home.component'
-import {Routes, Route} from 'react-router-dom'
-import Navbar from './routes/navbar/navbar.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import { Routes, Route } from 'react-router-dom';
 
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
 
-const Shop = () =>{
-  return(
-    <div>
-      <div>
-        This is SHOPPING
-      </div>
-    </div>
-  );
-}
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
-
   return (
     <Routes>
-      <Route path='/' element={<Navbar/>}>
-        <Route index element={<Home/>}/>
-        <Route path='shop' element={<Shop/>}/>
-        <Route path='signin' element={<SignIn/>}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
-
 };
 
 export default App;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,33 +4,42 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyB7FDPQLVT7lyVdrBa_611M_AjR0YOCLF4",
+  authDomain: "crwn-clothing-app-94065.firebaseapp.com",
+  projectId: "crwn-clothing-app-94065",
+  storageBucket: "crwn-clothing-app-94065.appspot.com",
+  messagingSenderId: "92874466112",
+  appId: "1:92874466112:web:9b05289d19dc3cea9f10a5"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleProvider.setCustomParameters({
   prompt: 'select_account',
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
   const userDocRef = doc(db, 'users', userAuth.uid);
 
   const userSnapshot = await getDoc(userDocRef);
@@ -44,6 +53,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the user', error.message);
@@ -53,9 +63,14 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   return userDocRef;
 };
 
-export const createAuthUserWithEmailAndPassword=async(email,pass)=>{
-  if(!email || !pass)return;
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
-  return await createUserWithEmailAndPassword(auth, email,pass);
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 
-}
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
